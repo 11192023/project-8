dbe9ed39817beacc3d45b7a83291c33ca784b2f1
hung tommy <doviethung.5409@gmail.com>
2024-09-04 20:57:30 +0700

    responsive for navbar 3

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -113,6 +113,7 @@ function handleActiveMenu() {
             if (!items.length) return;
 
             removeActive(menu);
+            if (window.innerWidth > 991)
             items[0].classList.add(activeClass);
 
             Array.from(items).forEach((item) => {
@@ -120,6 +121,12 @@ function handleActiveMenu() {
                     if (window.innerWidth <= 991) return;
                     removeActive(menu);
                     item.classList.add(activeClass);
+                };
+                item.onclick = () => {
+                    if (window.innerWidth > 991) return;
+                    removeActive(menu);
+                    item.classList.add(activeClass);
+                    item.scrollIntoView();
                 };
             });
         });
